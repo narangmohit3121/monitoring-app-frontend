9c426a994d88b21cb3947a289b030a23480f97f3
narangmohit <mohit.narang@bts.com>
2022-06-15 13:44:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LoginService } from './Login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges, OnInit, OnDestroy {
   title = 'monitoring-app';
+  // tabs : string[] = ["API Results","Functional Results"]
+  tabs: string[] = ["apiResults", "functionalResults"]
+  currentTab: string = "";
+  isApiTabSelected: boolean = true;
+  isUserAuthenticated: boolean = true;
+  userAuthSubscription: Subscription;
+
+  constructor(private loginService: LoginService) {
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(`Changed tab to ${this.currentTab}`)
+  }
+
+  ngOnInit(): void {
+    this.userAuthSubscription = this.loginService.isUserLoggedIn.subscribe((isloginSucessful) => {
+      console.log('Inside subscription for authentication in App component')
+      // this.isUserAuthenticated = isloginSucessful;
+    })
+  }
+
+  selectedTab(event: Event, tabName: string) {
+    console.log(tabName);
+    this.currentTab = tabName;
+    if (tabName == "API Results") {
+      this.isApiTabSelected = true;
+    }
+    else {
+      this.isApiTabSelected = false;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.userAuthSubscription.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http"
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { ApiResults } from 'src/app/apiResults/api.results.component';
 
 import { AppComponent } from './app.component';
+import { FunctionalResultsComponent } from './functionalResults/functional-results/functional-results.component';
+import { ApiResultComponent } from './apiResults/api-result/api-result.component';
+import { FilterPipe } from './apiResults/filter.pipe';
+import { FormsModule } from '@angular/forms';
+import { ChartModule } from 'angular-highcharts';
+import { SignUpComponent } from './login/sign-up/sign-up.component';
+import { SignInComponent } from './login/sign-in/sign-in.component';
+import { ApiJobDetailsComponent } from './jobDetails/api-job-details/api-job-details.component';
+import { AlertComponent } from './apiResults/alert/alert.component';
+import { PlaceholderDirective } from './apiResults/alert/placeholder.directive';
 
+
+let routes: Routes = [
+  { path: '', component: SignInComponent },
+  { path: "apiResults", component: ApiResults },
+  { path: "functionalResults", component: FunctionalResultsComponent },
+  { path: "apiResults/:jobName", component: ApiResultComponent },
+  { path: "login", component: SignUpComponent },
+  { path: "apiJobDetail", component: ApiJobDetailsComponent }
+
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApiResults,
+    FunctionalResultsComponent,
+    ApiResultComponent,
+    FilterPipe,
+    SignUpComponent,
+    SignInComponent,
+    ApiJobDetailsComponent,
+    AlertComponent,
+    PlaceholderDirective,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    FormsModule,
+    ChartModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [AlertComponent],
 })
 export class AppModule { }
